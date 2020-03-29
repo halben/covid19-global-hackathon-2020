ea61cbf4b0622f2dc6093d6933c1d1d631278111
kou.lor <roluok87@gmail.com>
2020-03-29 15:51:47 -0500

    deploy

---
+++ precache-manifest.1aa525764db99a9a7792ca3227eb3fd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5ad78bbc357e26a219",
+    "url": "/covid19-global-hackathon-2020/css/chunk-vendors.59f8d984.css"
+  },
+  {
+    "revision": "46af50b3f52c6b201df384f00a8aed13",
+    "url": "/covid19-global-hackathon-2020/img/logo.46af50b3.png"
+  },
+  {
+    "revision": "4f90fa933701ef3fd632b58feb3f0332",
+    "url": "/covid19-global-hackathon-2020/index.html"
+  },
+  {
+    "revision": "a013f9b96ec2d2381128",
+    "url": "/covid19-global-hackathon-2020/js/app.b2d8258e.js"
+  },
+  {
+    "revision": "da5ad78bbc357e26a219",
+    "url": "/covid19-global-hackathon-2020/js/chunk-vendors.d25dcbaa.js"
+  },
+  {
+    "revision": "0e1314fed56d89334c8e81986b268682",
+    "url": "/covid19-global-hackathon-2020/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid19-global-hackathon-2020/robots.txt"
+  }
+]);
